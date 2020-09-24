023c65a87a5399e46b4bf04a3244ca47dd740233
Matt <mws262@cornell.edu>
2020-09-24 11:59:35 -0700

    bike optim working for simple tests

--- examples/bike/bike.cxx
+++ examples/bike/bike.cxx
@@ -5,7 +5,7 @@ const adouble g = 9.81;
 const adouble Ixx = 0;
 const adouble Iyy = 0;
 const adouble Izz = 0;
-const adouble Ixy = 0;
+const adouble Ixz = 0;
 const adouble wheelbase = 1.05;
 const adouble com_to_rear = 0.546;
 const adouble com_height = 0.58;
@@ -19,7 +19,7 @@ adouble psid_eqn(adouble& phi, adouble& V, adouble& delta) {
     return -(V*sin(delta))/(wheelbase*cos(delta)*cos(phi));
 }
 
-adouble Vd_eqn(adouble& phi, adouble& V, adouble& phid, adouble& delta) {
+adouble Vd_eqn(adouble& phi, adouble& V, adouble& phid, adouble& delta, adouble& deltad, adouble& torque_front, adouble& torque_rear) {
   adouble t2 = cos(delta);
   adouble t3 = cos(phi);
   adouble t4 = sin(delta);
@@ -51,7 +51,7 @@ adouble Vd_eqn(adouble& phi, adouble& V, adouble& phid, adouble& delta) {
  return (Ixx*fwheel_radius*t15*t17*t20*torque_rear+Ixx*rwheel_radius*t15*t27*t29*torque_front-Ixx*rwheel_radius*t15*t18*t19*t29*torque_front+Ixx*rwheel_radius*t15*t18*t21*t29*torque_front+fwheel_radius*m*t8*t15*t17*t20*torque_rear+m*rwheel_radius*t8*t15*t27*t29*torque_front-Ixz*Iyy*fwheel_radius*rwheel_radius*t5*t7*t19*t23+Ixz*Izz*fwheel_radius*rwheel_radius*t5*t7*t19*t23+V*deltad*fwheel_radius*rwheel_radius*t6*t20*t23*wheelbase-m*rwheel_radius*t8*t15*t18*t19*t29*torque_front+m*rwheel_radius*t8*t15*t18*t21*t29*torque_front+Ixx*Ixz*fwheel_radius*rwheel_radius*t4*t5*t13*t14*t27-Ixz*fwheel_radius*m*rwheel_radius*t5*t7*t8*t19*t23+V*deltad*fwheel_radius*rwheel_radius*t4*t6*t16*t20*wheelbase-com_to_rear*fwheel_radius*rwheel_radius*t5*t7*t9*t12*t19*t23-Ixx*Iyy*V*deltad*fwheel_radius*rwheel_radius*t3*t23*wheelbase+Ixx*Iyy*V*deltad*fwheel_radius*rwheel_radius*t20*t23*wheelbase-Ixx*Izz*V*deltad*fwheel_radius*rwheel_radius*t20*t23*wheelbase+Ixx*Iyy*V*deltad*fwheel_radius*rwheel_radius*t3*t4*t26*wheelbase+Ixx*Iyy*V*deltad*fwheel_radius*rwheel_radius*t4*t16*t20*wheelbase+Ixx*Izz*V*deltad*fwheel_radius*rwheel_radius*t4*t20*t26*wheelbase-Ixx*Iyy*V*fwheel_radius*phid*rwheel_radius*t2*t5*t22*wheelbase-Ixx*V*deltad*fwheel_radius*m*rwheel_radius*t3*t11*t23*wheelbase-Iyy*V*deltad*fwheel_radius*m*rwheel_radius*t3*t8*t23*wheelbase+Iyy*V*deltad*fwheel_radius*m*rwheel_radius*t8*t20*t23*wheelbase-Izz*V*deltad*fwheel_radius*m*rwheel_radius*t8*t20*t23*wheelbase-Iyy*com_height*com_to_rear*fwheel_radius*m*rwheel_radius*t5*t7*t19*t23+Izz*com_height*com_to_rear*fwheel_radius*m*rwheel_radius*t5*t7*t19*t23-Ixx*V*fwheel_radius*m*phid*rwheel_radius*t2*t5*t11*wheelbase+Ixx*V*fwheel_radius*m*phid*rwheel_radius*t5*t11*t17*wheelbase+Ixz*com_height*fwheel_radius*m*rwheel_radius*t2*t7*t20*t22*wheelbase+Ixz*fwheel_radius*m*rwheel_radius*t4*t5*t8*t13*t14*t27+V*deltad*fwheel_radius*rwheel_radius*t5*t9*t12*t14*t17*t19+V*deltad*fwheel_radius*rwheel_radius*t4*t8*t11*t12*t20*wheelbase-V*deltad*fwheel_radius*rwheel_radius*t3*t8*t11*t12*t23*wheelbase-V*deltad*fwheel_radius*rwheel_radius*t3*t10*t12*t23*t24*wheelbase+V*fwheel_radius*phid*rwheel_radius*t4*t9*t12*t14*t16*t20*2.0-V*fwheel_radius*phid*rwheel_radius*t2*t5*t8*t11*t12*wheelbase+V*fwheel_radius*phid*rwheel_radius*t5*t8*t11*t12*t17*wheelbase+V*fwheel_radius*phid*rwheel_radius*t2*t5*t6*t19*t22*wheelbase-V*fwheel_radius*phid*rwheel_radius*t2*t10*t12*t22*t25*wheelbase+com_to_rear*fwheel_radius*rwheel_radius*t4*t5*t9*t12*t13*t14*t27+com_to_rear*fwheel_radius*rwheel_radius*t2*t7*t8*t12*t20*t22*wheelbase+V*deltad*fwheel_radius*rwheel_radius*t2*t5*t9*t12*t14*t19*t22+V*deltad*fwheel_radius*rwheel_radius*t3*t4*t8*t11*t12*t26*wheelbase+V*deltad*fwheel_radius*rwheel_radius*t3*t4*t10*t12*t24*t26*wheelbase+V*fwheel_radius*phid*rwheel_radius*t3*t4*t9*t12*t14*t16*t24-V*fwheel_radius*phid*rwheel_radius*t2*t5*t10*t12*t19*t22*wheelbase*2.0+com_to_rear*fwheel_radius*g*rwheel_radius*t4*t5*t8*t12*t14*t20*t26+Ixz*V*com_height*com_to_rear*deltad*fwheel_radius*m*rwheel_radius*t4*t20*wheelbase+Ixz*V*com_height*com_to_rear*deltad*fwheel_radius*m*rwheel_radius*t20*t23*wheelbase-Ixx*Iyy*V*fwheel_radius*phid*rwheel_radius*t2*t5*t19*t22*wheelbase+Ixx*Izz*V*fwheel_radius*phid*rwheel_radius*t2*t5*t19*t22*wheelbase+Ixx*V*com_height*deltad*fwheel_radius*m*rwheel_radius*t5*t14*t17*t19+Ixx*V*com_height*fwheel_radius*m*phid*rwheel_radius*t4*t14*t16*t20*2.0+Ixx*V*deltad*fwheel_radius*m*rwheel_radius*t3*t4*t11*t26*wheelbase-Ixx*V*deltad*fwheel_radius*m*rwheel_radius*t3*t8*t23*t24*wheelbase+Iyy*V*deltad*fwheel_radius*m*rwheel_radius*t3*t4*t8*t26*wheelbase+Iyy*V*deltad*fwheel_radius*m*rwheel_radius*t4*t8*t16*t20*wheelbase+Izz*V*deltad*fwheel_radius*m*rwheel_radius*t4*t8*t20*t26*wheelbase+Ixx*com_height*com_to_rear*fwheel_radius*m*rwheel_radius*t4*t5*t13*t14*t27-Ixx*V*fwheel_radius*m*phid*rwheel_radius*t2*t8*t22*t25*wheelbase-Iyy*V*fwheel_radius*m*phid*rwheel_radius*t2*t5*t8*t22*wheelbase+Ixz*com_height*fwheel_radius*g*m*rwheel_radius*t4*t5*t14*t20*t26+Ixz*V*com_height*com_to_rear*deltad*fwheel_radius*m*rwheel_radius*t4*t16*t20*wheelbase+Ixx*V*com_height*deltad*fwheel_radius*m*rwheel_radius*t2*t5*t14*t19*t22+Ixx*V*com_height*fwheel_radius*m*phid*rwheel_radius*t3*t4*t14*t16*t24+Ixx*V*deltad*fwheel_radius*m*rwheel_radius*t3*t4*t8*t24*t26*wheelbase-Ixx*V*fwheel_radius*m*phid*rwheel_radius*t2*t5*t8*t19*t22*wheelbase*2.0-Iyy*V*fwheel_radius*m*phid*rwheel_radius*t2*t5*t8*t19*t22*wheelbase+Izz*V*fwheel_radius*m*phid*rwheel_radius*t2*t5*t8*t19*t22*wheelbase+V*fwheel_radius*phid*rwheel_radius*t2*t5*t8*t11*t12*t19*t22*wheelbase+Ixz*V*com_height*com_to_rear*fwheel_radius*m*phid*rwheel_radius*t2*t5*t19*t22*wheelbase*2.0)/(t2*t3*wheelbase*(Ixx*Iyy*fwheel_radius*rwheel_radius*t22+Ixx*fwheel_radius*m*rwheel_radius*t11+fwheel_radius*rwheel_radius*t8*t11*t12-fwheel_radius*rwheel_radius*t6*t19*t22+fwheel_radius*rwheel_radius*t8*t11*t12*t26+fwheel_radius*rwheel_radius*t8*t12*t14*t27+fwheel_radius*rwheel_radius*t10*t12*t22*t24-Ixx*Iyy*fwheel_radius*rwheel_radius*t19*t22+Ixx*Izz*fwheel_radius*rwheel_radius*t19*t22+Ixx*fwheel_radius*m*rwheel_radius*t11*t26+Ixx*fwheel_radius*m*rwheel_radius*t14*t27+Iyy*fwheel_radius*m*rwheel_radius*t8*t22+Ixx*fwheel_radius*m*rwheel_radius*t8*t22*t24-Iyy*fwheel_radius*m*rwheel_radius*t8*t19*t22+Izz*fwheel_radius*m*rwheel_radius*t8*t19*t22-fwheel_radius*rwheel_radius*t8*t11*t12*t19*t22-Ixz*com_height*com_to_rear*fwheel_radius*m*rwheel_radius*t19*t22*2.0-fwheel_radius*rwheel_radius*t2*t3*t4*t5*t9*t12*wheelbase*2.0-Ixx*com_height*fwheel_radius*m*rwheel_radius*t2*t3*t4*t5*wheelbase*2.0));
 }
 
-adouble phidd_eqn(adouble& phi, adouble& V, adouble& phid, adouble& delta) {
+adouble phidd_eqn(adouble& phi, adouble& V, adouble& phid, adouble& delta, adouble& deltad, adouble& torque_front, adouble& torque_rear) {
   /*
    state -- state vector, order [phi, V, phid, delta]
 % controls -- control vector, order [torque_front torque_rear, deltad]
@@ -112,7 +112,7 @@ adouble integrand_cost(adouble* states, adouble* controls,
                        adouble* parameters, adouble& time, adouble* xad,
                        int iphase, Workspace* workspace)
 {
-    return  controls[0] * controls[0] + controls[1] * controls[1] + controls[2] * controls[2]; // Squared torque front, rear, and steer acceleration.
+    return states[4]*0. ;//controls[0] * controls[0] + controls[1] * controls[1] + controls[2] * controls[2]; // Squared torque front, rear, and steer acceleration.
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -139,12 +139,14 @@ void dae(adouble* derivatives, adouble* path, adouble* states,
    adouble deltadd = controls[2];
    
    derivatives[0] = phid;
-   derivatives[1] = Vd_eqn(phi, V, phid, delta); 
-   derivatives[2] = phidd_eqn(phi, V, phid, delta); 
+   derivatives[1] = Vd_eqn(phi, V, phid, delta,  deltad,  tf,  tr); 
+   // cout << phi << ", " << V << ", " << phid << ", " << delta << ", " << deltad << ", " << tf << ", " << tr << "\n";
+   derivatives[2] = phidd_eqn(phi, V, phid, delta,  deltad,  tf,  tr); 
    derivatives[3] = deltad;
    derivatives[4] = deltadd;
    derivatives[5] = psid_eqn(phi, V, delta);
    derivatives[6] = V * cos(psi);
+   // cout << derivatives[5] << "\n";
    derivatives[7] = V * sin(psi);
 }
 
@@ -247,7 +249,8 @@ int main(void)
     problem.phases(1).ncontrols 				= 3;
     problem.phases(1).nevents   				= 16;
     problem.phases(1).npath     				= 0;
-    problem.phases(1).nodes           << 40;
+    int nodes = 10;
+    problem.phases(1).nodes           << nodes;
 
     psopt_level2_setup(problem, algorithm);
 
@@ -265,32 +268,32 @@ int main(void)
 
     problem.phases(1).bounds.lower.states(0) = -1.57; // phi
     problem.phases(1).bounds.lower.states(1) = 0.0; // V
-    problem.phases(1).bounds.lower.states(2) = -5.0; // phid
+    problem.phases(1).bounds.lower.states(2) = -30.0; // phid
     problem.phases(1).bounds.lower.states(3) = -1.57; // delta
-    problem.phases(1).bounds.lower.states(4) = -5.0; // deltad
+    problem.phases(1).bounds.lower.states(4) = -30.0; // deltad
     problem.phases(1).bounds.lower.states(5) = -3.142; // psi
     problem.phases(1).bounds.lower.states(6) = -20.0; // xr
     problem.phases(1).bounds.lower.states(7) = -20.0; // yr
 
     problem.phases(1).bounds.upper.states(0) = 1.57;
-    problem.phases(1).bounds.upper.states(1) = 6.0;
-    problem.phases(1).bounds.upper.states(2) = 5.0;
+    problem.phases(1).bounds.upper.states(1) = 2.0;
+    problem.phases(1).bounds.upper.states(2) = 30.0;
     problem.phases(1).bounds.upper.states(3) = 1.57;
-    problem.phases(1).bounds.upper.states(4) = 5.0;
+    problem.phases(1).bounds.upper.states(4) = 30.0;
     problem.phases(1).bounds.upper.states(5) = 3.142;
     problem.phases(1).bounds.upper.states(6) = 20.0;
     problem.phases(1).bounds.upper.states(7) = 20.0;
 
     problem.phases(1).bounds.lower.controls(0) = -20.0; // tfront
     problem.phases(1).bounds.lower.controls(1) = -20.0; // trear
-    problem.phases(1).bounds.lower.controls(1) = -30.0; // deltadd
+    problem.phases(1).bounds.lower.controls(2) = -5.; // deltadd
 
     problem.phases(1).bounds.upper.controls(0) = 20.0;
     problem.phases(1).bounds.upper.controls(1) = 20.0;
-    problem.phases(1).bounds.upper.controls(1) = 30.0;
+    problem.phases(1).bounds.upper.controls(2) = 5.0;
 
     // Initial.
-    problem.phases(1).bounds.lower.events(0) = 0.0;
+    problem.phases(1).bounds.lower.events(0) = 0.05;
     problem.phases(1).bounds.lower.events(1) = 2.0;
     problem.phases(1).bounds.lower.events(2) = 0.0;
     problem.phases(1).bounds.lower.events(3) = 0.0;
@@ -306,7 +309,7 @@ int main(void)
     problem.phases(1).bounds.lower.events(11) = 1.0;
     problem.phases(1).bounds.lower.events(12) = 0.0;
     problem.phases(1).bounds.lower.events(13) = 0.0;
-    problem.phases(1).bounds.lower.events(14) = 6.0;
+    problem.phases(1).bounds.lower.events(14) = 4.0;
     problem.phases(1).bounds.lower.events(15) = 0.0;
 
     problem.phases(1).bounds.upper.events = problem.phases(1).bounds.lower.events;
@@ -315,8 +318,8 @@ int main(void)
     problem.phases(1).bounds.lower.StartTime    = 0.0;
     problem.phases(1).bounds.upper.StartTime    = 0.0;
 
-    problem.phases(1).bounds.lower.EndTime      = 1.0;
-    problem.phases(1).bounds.upper.EndTime      = 10.0;
+    problem.phases(1).bounds.lower.EndTime      = 3.;
+    problem.phases(1).bounds.upper.EndTime      = 6.;
 
 
 ////////////////////////////////////////////////////////////////////////////
@@ -337,20 +340,20 @@ int main(void)
 ////////////////////////////////////////////////////////////////////////////
 
 
-    MatrixXd x0(4, 40);
+    MatrixXd x0(8, nodes);
 
-    x0 <<  linspace(0.0, 0.0, 40), // phi
-           linspace(2.0, 0.0, 40), // V
-           linspace(0.0, 0.0, 40), // phid
-           linspace(0.0, 1.0, 40), // delta
-           linspace(0.0, 0.0, 40), // deltad
-           linspace(0.0, 0.0, 40), // yaw
-           linspace(0.0, 6.0, 40), // xr
-           linspace(0.0, 0.0, 40); // yr
+    x0 <<  linspace(0.0, 0.0, nodes), // phi
+           linspace(2.0, 0.0, nodes), // V
+           linspace(0.0, 0.0, nodes), // phid
+           linspace(0.0, 1.0, nodes), // delta
+           linspace(0.0, 0.0, nodes), // deltad
+           linspace(0.0, 0.0, nodes), // yaw
+           linspace(0.0, 10.0, nodes), // xr
+           linspace(0.0, 0.0, nodes); // yr
 
-    problem.phases(1).guess.controls       = zeros(3, 40);
+    problem.phases(1).guess.controls       = zeros(3, nodes);
     problem.phases(1).guess.states         = x0;
-    problem.phases(1).guess.time           = linspace(0.0, 3.0, 40);
+    problem.phases(1).guess.time           = linspace(0.0, 3.0, nodes);
 
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////  Enter algorithm options  //////////////////////////////
@@ -362,7 +365,7 @@ int main(void)
     algorithm.derivatives                 = "automatic";
     algorithm.nlp_iter_max                = 1000;
     algorithm.nlp_tolerance               = 1.e-6;
-
+    algorithm.collocation_method = "Hermite-Simpson";
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////  Now call PSOPT to solve the problem   /////////////////
 ////////////////////////////////////////////////////////////////////////////
